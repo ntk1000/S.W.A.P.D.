748994a81b0efb64852fb463c3b89ba4c4631762
ntk1000 <jun22011982@gmail.com>
2014-09-08 00:20:00 +0900

    validate?

--- sample/ums/forms.py
+++ sample/ums/forms.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.forms.extras import widgets
+import datetime
 from ums.models import DemoUser
 
 class DemoUserForm(forms.ModelForm):
-	username = forms.CharField(label=u'お名前')
-	email = forms.EmailField(label=u'メールアドレス')
-	password = forms.CharField(label=u'パスワード',widget=forms.PasswordInput())
+	help_base=u'{0}文字以内で入力してください'
+	username_len=30
+	email_len=75
+	password_len=128
+
+	username = forms.CharField(label=u'お名前',help_text=help_base.format(username_len),max_length=username_len)
+	email = forms.EmailField(label=u'メールアドレス',help_text=u'有効なメールアドレスを'+help_base.format(email_len),max_length=email_len)
+	password = forms.CharField(label=u'パスワード',help_text=help_base.format(password_len),widget=forms.PasswordInput(),max_length=password_len)
 	url = forms.URLField(label=u'URL',required=False)
-	birth = forms.DateField(label=u'生年月日',widget=widgets.SelectDateWidget(),required=False)
+	birth = forms.DateField(label=u'生年月日',widget=widgets.SelectDateWidget(years=range(1900, datetime.datetime.today().year+1)),required=False)
 	class Meta:
 		model = DemoUser
 		fields = ('username','email','password','url','birth')
