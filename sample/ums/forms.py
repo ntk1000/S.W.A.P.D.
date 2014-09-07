2626077d61fd1d4a002f543bc21f7bb5e2f93c94
ntk1000 <jun22011982@gmail.com>
2014-09-07 23:25:06 +0900

    widget

--- sample/ums/forms.py
+++ sample/ums/forms.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.forms.extras import widgets
 from ums.models import DemoUser
 
 class DemoUserForm(forms.ModelForm):
+	username = forms.CharField()
+	email = forms.EmailField()
+	password = forms.CharField(widget=forms.PasswordInput())
+	birth = forms.DateField(widget=widgets.SelectDateWidget())
 	class Meta:
 		model = DemoUser
-		fields = ('username','email','password','url','birthyear','birthmonth')
+		fields = ('username','email','password','url','birth')
--- sample/ums/models.py
+++ sample/ums/models.py
@@ -6,8 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 class DemoUser(AbstractUser):
 	"""DemoUser Model Class."""
 	url = models.URLField(u'URL',blank=True,null=True)
-	birthyear = models.PositiveSmallIntegerField(u'生年',blank=True,null=True,max_length=4)
-	birthmonth = models.PositiveSmallIntegerField(u'月',blank=True,null=True,max_length=2)
+	birth = models.DateField(u'生年月',blank=True,null=True)
 
 	USERNAME_FIELD = 'username'
 	REQUIRED_FIELDS = ['email', 'password']
