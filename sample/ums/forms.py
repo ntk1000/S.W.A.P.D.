5c5c127933480d559d463a71fc6b2713c9f53b84
ntk1000 <jun22011982@gmail.com>
2014-09-07 23:54:23 +0900

    required

--- sample/ums/forms.py
+++ sample/ums/forms.py
@@ -4,10 +4,11 @@ from django.forms.extras import widgets
 from ums.models import DemoUser
 
 class DemoUserForm(forms.ModelForm):
-	username = forms.CharField()
-	email = forms.EmailField()
-	password = forms.CharField(widget=forms.PasswordInput())
-	birth = forms.DateField(widget=widgets.SelectDateWidget())
+	username = forms.CharField(label=u'お名前')
+	email = forms.EmailField(label=u'メールアドレス')
+	password = forms.CharField(label=u'パスワード',widget=forms.PasswordInput())
+	url = forms.URLField(label=u'URL',required=False)
+	birth = forms.DateField(label=u'生年月日',widget=widgets.SelectDateWidget(),required=False)
 	class Meta:
 		model = DemoUser
 		fields = ('username','email','password','url','birth')
