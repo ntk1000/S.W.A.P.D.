0cddf584d0402c23dfa25c357a2ce683ba76889e
ntk1000 <jun22011982@gmail.com>
2014-09-08 19:09:07 +0900

    password保存

--- sample/ums/forms.py
+++ sample/ums/forms.py
@@ -12,23 +12,23 @@ class DemoUserForm(forms.ModelForm):
 
 	username = forms.CharField(label=u'お名前',help_text=help_base.format(username_len),max_length=username_len)
 	email = forms.EmailField(label=u'メールアドレス',help_text=u'有効なメールアドレスを'+help_base.format(email_len),max_length=email_len)
-	password1 = forms.CharField(label=u'パスワード',help_text=help_base.format(password_len),widget=forms.PasswordInput(),max_length=password_len)
+	password = forms.CharField(label=u'パスワード',help_text=help_base.format(password_len),widget=forms.PasswordInput(),max_length=password_len)
 	password2 = forms.CharField(label=u'パスワード(再)',widget=forms.PasswordInput(),max_length=password_len)
 	url = forms.URLField(label=u'URL',required=False)
 	birth = forms.DateField(label=u'生年月日',widget=widgets.SelectDateWidget(years=range(1900, datetime.datetime.today().year+1)),required=False)
 
 	class Meta:
 		model = DemoUser
-		fields = ('username','email','password1','password2','url','birth')
+		fields = ('username','email','password','password2','url','birth')
 
-	def __init__(self, *args, **kwargs):
-		super(DemoUserForm, self).__init__(*args, **kwargs)
+	# def __init__(self, *args, **kwargs):
+	# 	super(DemoUserForm, self).__init__(*args, **kwargs)
 
 	def clean_password2(self):
-		password1 = self.cleaned_data.get('password1')
+		password = self.cleaned_data.get('password')
 		password2 = self.cleaned_data.get('password2')
-		if password1 and password2:
-			if password1 != password2:
+		if password and password2:
+			if password != password2:
 				raise forms.ValidationError("パスワードが一致しません")
 		return password2
 
